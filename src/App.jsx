436e76f331afc5a793e6fec8c713b01b6cacb753
SeonHyungJo <seonhyung.jo@gmail.com>
2021-02-24 01:33:03 +0900

    update: add 노션 이력서 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
-import './App.css'
-
-import "./prism-tomorrow.css"; // only needed for code highlighting
+import React, { useEffect, useState } from 'react'
 import { NotionRenderer } from "react-notion";
+import axios from 'axios';
+
+import './App.css'
+import "./prism-tomorrow.css"; // only needed for code highlighting
 
 import response from "./page.json"; // https://www.notion.so/api/v3/loadPageChunk
 
-const blockMap = response;
-
 function App() {
+  const [blockMap, setBlockMap] = useState({})
+
+  useEffect(() => {
+    axios.get('https://worker-test.zaritalk.workers.dev/v1/page/46d9612b457b4c23bc073105e69d15e3').then((res) => {
+      setBlockMap(res.data)
+    })
+  }, [])
+
   return (
     <div className="App">
       <div style={{ maxWidth: 768 }}>
